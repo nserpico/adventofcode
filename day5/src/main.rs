5923b0102b868014c8620f4947a9a553eb0a3341
Nate Serpico <nathaniel.serpico@gmail.com>
2023-12-05 17:44:36 -0500

    clean up

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,12 +1,9 @@
 use rayon::prelude::*;
 use std::{collections::HashMap, fs::read_to_string};
-use std::sync::atomic::{AtomicU64, Ordering};
-use std::sync::Arc;
 
 fn read(filename: &str) -> String {
     read_to_string(filename).unwrap()
 }
-// dest source range
 
 fn do_map(seed: u64, digits: &Vec<u64>) -> u64 {
     if seed >= digits[1] && seed < digits[1] + digits[2] {
@@ -48,18 +45,13 @@ fn main() {
         .into_par_iter()
         .map(|s| s.parse::<u64>().unwrap())
         .collect::<Vec<u64>>();
-    println!("read seeds");
     let maps = &chunks[1..];
     let processed_maps = maps
         .into_par_iter()
         .map(|m| process_map(m))
         .collect::<Vec<Vec<Vec<u64>>>>();
-    println!("read maps");
     let exp_seeds = expand_seeds(seeds);
-    println!("starting!");
     let total = exp_seeds.len();
-    println!("{}", total);
-    let count = Arc::new(AtomicU64::new(0));
     let result = exp_seeds
         .into_par_iter()
         .map(|seed| {
@@ -73,11 +65,6 @@ fn main() {
                     }
                 }
             }
-            let c = count.fetch_add(1, Ordering::SeqCst);
-            if c % 10000 == 0 {
-                println!("{:?}/{}", c, total);
-            }
-            // println!("{}", seed_transformer);
             seed_transformer
         })
         .min();
